4328ec0064f5b137baecbd1a093bbf014436e2a3
hartimo <adriano@hartimo.com.br>
2018-04-18 21:45:48 -0300

    Aula 18/04/2018

--- clientes.js
+++ clientes.js
@@ -28,7 +28,49 @@ $(function(){
                         +'</tr>';
 
                 $("tbody").append(tr);
-            });
-        });
+            });//Fim forEach
+        });//Fim JSON
     /*});*/
+    
+    $("fieldset button").click(function(){
+
+        var nome = $("input[name=nome]").val();
+        var email = $("input[name=email]").val();
+        var cep = $("input[name=cep]").val();
+        var dataNasc = $("input[name=data-nasc]").val();
+
+        var tr = '<tr>'
+        +'<td>'+ nome +'</td>'
+        +'<td>'+ email +'</td>'
+        +'<td>'+ cep +'</td>'
+        +'<td>'+ dataNasc +'</td>'
+        +'</tr>';
+
+    var tr2 = $(tr).hide().fadeIn();//Cria ao evento de aparecer, hide oculta e fadeInt Mostra.
+        $("tbody").append(tr2);
+    });//Fim do click - Botão adicionar
+
+    $("input[name=cep]").keydown(function(ev){
+        if ((ev.keyCode >= 48 && ev.keyCode <= 57) || ev.keyCode == 8)
+        {
+            return True;
+        } else {
+            return false;
+        }
+    });//Fim Keydown
+
+    $("input[name=data-nasc]").datepicker({
+        dateFormat: "dd/mm/yy"
+    })
+
+    $("input[name=email]").blur(function(){
+        var valor = $(this).val();
+        var regex = /^[a-zA-Z0-9\.\-_]*@[a-z0-9]*\.[a-z\.]*$/gm;
+        
+        if(regex.text(valor) == false)
+        {
+            alert("email inválido");
+        }
+    });
+
 });
